85d26256d6c525478e58c95ae6917745703b1e63
Tyler Zhu <67493075+zetachan17@users.noreply.github.com>
2021-03-30 16:18:04 -0400

    Q1
    Not hard tbh. Surprised to see the runtime beats 100% of the submissions.

---
+++ 1.two-sum.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    int[] ans = {i, j};
+                    return ans;
+                }
+            }
+        }
+        return null;
+    }
+}
+// @lc code=end
+
