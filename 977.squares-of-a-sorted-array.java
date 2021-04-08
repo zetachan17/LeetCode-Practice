45da02568a7ce20d4a9ecd8be36db2dc0d2e627b
Tyler Zhu <67493075+zetachan17@users.noreply.github.com>
2021-04-08 10:42:35 -0400

    EZ

---
+++ 977.squares-of-a-sorted-array.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=977 lang=java
+ *
+ * [977] Squares of a Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) Math.pow(nums[i], 2);
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+// @lc code=end
+
